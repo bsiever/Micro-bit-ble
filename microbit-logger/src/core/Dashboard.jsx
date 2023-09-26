b12e1a55c555239af4282166600d188195dad221
Joshua Staples <104746227+jwstaples1@users.noreply.github.com>
2023-09-25 23:37:46 -0400

    Connect Microbit Functionality (#3)
    * creates navbar buttons
    
    * import Bill's code and add handler to connect button
    
    * setup app state and log microbit details when connected

--- microbit-logger/src/core/Dashboard.jsx
+++ microbit-logger/src/core/Dashboard.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
+import { uBitManager } from "../microbit/ubitwebblelog";
+
+export const MicrobitContext = React.createContext({})
 
 const Dashboard = () => {
+    const microbitManager = new uBitManager();
+    const [context, setContext] = useState({microbits: []})
+
     return (
+        <MicrobitContext.Provider value={{...context, microbitManager: microbitManager, updateContext: setContext}}>
         <div id='dashboard'><Navbar/><div id='display'></div></div>
+        </MicrobitContext.Provider>
     )
 }
 
