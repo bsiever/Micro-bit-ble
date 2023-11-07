277da614793531a722b7151b874080939edab0ba
jrsnowde <90734641+jrsnowde@users.noreply.github.com>
2023-11-07 00:55:00 -0500

    Landing page (#18)
    * Landing page add
    
    * updated css
    
    * yes
    
    * logo update

--- microbit-logger/src/core/DashboardView.jsx
+++ microbit-logger/src/core/DashboardView.jsx
@@ -5,6 +5,8 @@ import Loadingbar from "./Loadingbar";
 import DataTable from "../table/DataTable";
 import Graph from "../graph/Graph";
 import { EraseModal } from "./EraseModal";
+import React from 'react';
+import Accordion from './Accordion';
 
 const DashboardView = ({view}) => {
     const {microbits, microbitManager} = useContext(MicrobitContext);
@@ -20,17 +22,42 @@ const DashboardView = ({view}) => {
             microbitManager.removeEventListener('data-ready', ready)
         }
     }, [])
-
+    const accordionData = [
+        {
+          title: 'About the App',
+          content: `This is an application to view and download data from your Micro-Bit. Instead of having to manually reterive data off of your Micro-Bit, this app will connect to your
+          device, reterive the data, graph it, and offer a way to download your data to your device. This app was designed as part of Michigan Technological University's Senior Design program.`
+        },
+        {
+          title: 'How to Connect a Micro:Bit',
+          content: `Click the 'connect' button in the upper right corner on desktop or at the bottom of the screen on mobile to pair and/or connect a Micro-Bit. 
+          The data already on the Micro-Bit will be loaded, and any data collected while the device is connected will be continually added to the plot and table.`
+        },
+        
+      ];
+    
+      
+    
     return (
         <div>
-            {microbits.map((microbit) => {
+
+            {microbits.length>0 ? microbits.map((microbit) => {
                 return <div key={microbit.id}>
+                    
                     <EraseModal microbit={microbit} />
                     <PlotBar microbit={microbit}/>
                     {tableReady ? view != 'table' ? <Graph microbit={microbit}/> : <DataTable microbit={microbit}/> : <Loadingbar microbit={microbit}/>}
                 </div>
-            })}
-        </div>
+            }):
+            <div>
+            <h7>Micro:Bit Bluetooth Web Logger </h7>
+            <div className="accordion">
+              {accordionData.map(({ title, content }) => (
+                <Accordion title={title} content={content} />
+              ))}
+            </div>
+            </div>}
+          </div>
     )
 }
 
--- microbit-logger/src/core/NavbarWide.jsx
+++ microbit-logger/src/core/NavbarWide.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { MicrobitContext } from "./Dashboard";
-import icon from "../images/icon.png";
+import icon from "../images/logo.png";
 
 const NavbarWide = ({setView}) => {
     const {microbitManager, updateContext} = useContext(MicrobitContext);
@@ -18,11 +18,11 @@ const NavbarWide = ({setView}) => {
     return (
         <div id='navbar'>
             <div style={{display: 'flex', alignItems: 'center', fontSize: '25px', width: '50%'}}>
-                <div style={{width: '80px', height: '80px', border: '1px solid black', marginRight: '2%'}}><img src={icon} style={{width: '100%', height: '100%'}}/></div>
+                <div style={{width: '80px', height: '80px',  marginRight: '2%'}}><img src={icon} style={{width: '100%', height: '100%'}}/></div>
                 Micro:Bit Bluetooth Web Logger
                 <div style={{display: 'flex', justifyContent: 'space-around', width: '50%'}}>
-                    <div className="tab" onClick={() => setView('graph')}>Graphs</div>
-                    <div className="tab" onClick={() => setView('table')}>Data</div>
+                    <button className="tab" onClick={() => setView('graph')}>Graphs</button>
+                    <button className="tab" onClick={() => setView('table')}>Data</button>
                 </div>
             </div>
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end', width: '50%'}}>
