c094519180591a5996503a2ce8d0c246221a4467
Josh Staples <104746227+jwstaples1@users.noreply.github.com>
2024-04-15 20:16:16 -0400

    Sticky Header (#37)
    Makes the data table have a sticky header so it scrolls with you on the page
    
    Co-authored-by: AK Syracuse <73906958+AnnaSyracuse@users.noreply.github.com>

--- microbit-logger/src/table/DataTable.jsx
+++ microbit-logger/src/table/DataTable.jsx
@@ -1,40 +1,42 @@
 import Table from "react-bootstrap/Table";
 
-const DataTable = ({microbit}) => {
-        const headers = microbit.getHeaders();
-        const renderHeaders = headers.map((item, index) =>
-                                        <td key={"header_" + index}>{item}</td>);
+const DataTable = ({ microbit }) => {
+    const headers = microbit.getHeaders();
+    const renderHeaders = headers.map((item, index) =>
+        <th key={"header_" + index}>{item}</th>);
 
-        renderHeaders.splice(1, 1); // removes reboot time header
-        
-        const rows = microbit.getData();
+    renderHeaders.splice(1, 1); // removes reboot time header
 
-        return (
-		<div className='table'>
-			<Table bordered style={{fontSize: '1.25em', height: '100%', overflowY: 'scroll'}}>
+    const rows = microbit.getData();
+
+    return (
+        <div className='table'>
+            <Table bordered style={{ fontSize: '1.25em', height: '100%', overflowY: 'scroll' }}>
                 <thead>
-                    <tr style={{borderBottom: '1px solid black'}}>{renderHeaders}</tr>
+                    <tr style={{ borderBottom: '1px solid black', position: 'sticky', top: '0', background: 'white' }}>
+                        {renderHeaders}
+                    </tr>
                 </thead>
                 <tbody key={'table_body'}>
-                {rows.map((value, rowIndex) => {
-                    const displayedValues = [value[0], ...value.slice(2, value.length)]; // filters out reboot time value
-                    const reboot = value[1] === 'true';
-                    return (
-                        <tr key={"row_" + rowIndex}>
-                            {displayedValues.map((items, dataIndex) => {
-                                return (
-                                    <td style={{borderTop: reboot ? '10px solid grey' : undefined}} key={"row_" + rowIndex + "_" + dataIndex}>
-                                        {items}
-                                    </td>
-                                )
-                            })}
-                        </tr>
-                    )
-                })}
+                    {rows.map((value, rowIndex) => {
+                        const displayedValues = [value[0], ...value.slice(2, value.length)]; // filters out reboot time value
+                        const reboot = value[1] === 'true';
+                        return (
+                            <tr key={"row_" + rowIndex}>
+                                {displayedValues.map((items, dataIndex) => {
+                                    return (
+                                        <td style={{ borderTop: reboot ? '10px solid grey' : undefined }} key={"row_" + rowIndex + "_" + dataIndex}>
+                                            {items}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </Table>
-		</div>
-	)
+        </div>
+    )
 }
 
-export default DataTable
+export default DataTable;
